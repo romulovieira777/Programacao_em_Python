c6f96465d4159f7cc6611999e3249f3e9f4f346f
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-26 18:40:49 -0300

    Python na Prática

---
+++ Seção 02 - Python na Prática/variaveis.py
@@ -0,0 +1,57 @@
+# Criando Três Variáveis Inteiras
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+
+# Imprimindo o Valor na Tela
+print(numero)
+print(numero, numero_jogos, numero_convidados)
+
+
+# Criando Variáveis Float de (Ponto Flutuante)
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+
+# Imprimindo os Valores na Tela
+print(pi)
+print(numero_euler)
+print(escala_terremoto)
+
+
+# Criando uma Variável String
+letra = 'A'
+palavra_01 = 'Linguagem de Programação'
+palavra_02 = 'Python'
+
+# Imprimindo o Valor na Tela
+print(letra, palavra_01, palavra_02)
+
+
+# Concatenado uma palavra com a Variável Criada e Imprmindo na Tela
+print('Estou apredendo uma', palavra_01)
+
+
+# Concatenado uma palavra com a Variável Criada e Imprmindo na Tela
+print('Esta', palavra_01, 'se chama', palavra_02)
+
+
+# Criando uma Variável onde o Usuário Digita o Valor e Converte para Inteiro
+idade = int(input('Digite sua idade: '))
+
+# Imprimindo o Valor da Variável na Tela
+print('Sua idade é', idade)
+
+
+# Criando uma Variável onde o Usuário Digita o Valor e Converte para Float
+ph = float(input('Qual o pH do solo durante a última medição? '))
+
+# Imprindo o Valor da Variável na Tela
+print('O pH medido foi ', ph)
+
+
+# Criando uma Variável onde o Usuário Digita o Valor e Convert e para String
+nome = str(input('Qual é seu nome? '))
+
+# Imprimindo o Valor da Variável na Tela
+print('Seu nome é ', nome)
