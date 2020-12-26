bc5303801d3e0fc7af70f6cb8d55256a5dfd00c3
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-26 19:12:23 -0300

    Python na Prática

---
+++ Seção 02 - Python na Prática/operacoes_matematicas.py
@@ -0,0 +1,55 @@
+# Importando Biblioteca Matemática
+from math import sqrt
+
+
+# Criando Duas Variáveis
+a = 5
+b = 3
+
+# Imprimindo os Valores das Variáveis na Tela
+print(a, b)
+
+
+# Somando as Duas Variáveis e Imprimindo na Tela
+print('A soma é', a + b)
+
+
+# Subtraindo as Duas Variáveis e Imprimindo na Tela
+print('A subtração é', a - b)
+
+
+# Dividindo as Duas Variáveis e Imprimindo na Tela
+print('A divisão é', a / b)
+
+
+# Mutiplicando as Duas Variáveis e Imprimindo na Tela
+print('A multiplicação é', a * b)
+
+
+# Resto da Divisão das Duas Variáveis e Imprimindo na Tela
+print('O resto da divisão é', 10 % 3)
+
+
+# Elevendo ao Cubo as Duas Variáveis e Imprimindo na Tela
+print('5 elevado a 3 é', a ** b)
+
+
+# Elevendo ao Cubo as Duas Variáveis e Imprimindo na Tela e Utilizando Função Built-in
+print(sqrt(a))
+
+
+# Criando Três Variáveis
+casos_doenca = 134
+numero_habitantes = 34432
+casos_por_habitante = casos_doenca / numero_habitantes
+
+# Imprimindo os Valores na Tela
+print(casos_por_habitante)
+
+
+# Arredondando o Valor Impresso em Tela com Seis Casas Decimais Utilizando Função Built-in
+print(round(casos_por_habitante, 6))
+
+
+# Arredondando o Valor Impresso em Tela com Quatro Casas Decimais Utilizando Função Built-in
+print('O número de casos por habitante é de', round(casos_por_habitante, 4))
