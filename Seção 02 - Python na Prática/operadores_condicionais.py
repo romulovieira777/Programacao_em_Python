43225578cf5598e4768985b177fbd4fe83522fa4
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-27 12:25:29 -0300

    Python na Prática

---
+++ Seção 02 - Python na Prática/operadores_condicionais.py
@@ -0,0 +1,49 @@
+# Criando uma Estrutura de Condição com if
+if 5 > 3:
+    # Imprimindo o Valor na Tela se a Condição for Verdadeira
+    print('5 é maior que 3')
+# Imprimindo o Valor na Tela
+print('Teste')
+
+
+# Criando uma Estrutra de Condição com if e else
+if 5 > 6:
+    # Imprimindo o Valor na Tela se a Condição for Verdadeira
+    print('5 é maior')
+# Se a Primeira Condição não for Verdadeira Excuta o Else
+else:
+    # Imprimindo o Valor na Tela se a Condição for Verdadeira
+    print('5 não é maior')
+
+
+# Criando uma variável
+n = 3
+
+# Criando uma Estrutura de Condição com if e else
+if n == 4:
+    # Imprimindo o Valor na Tela se a Primeira Condição for Verdadeira
+    print('n é igual a 4')
+# Se a Primeira Condição não for Verdadeira Excuta o Else
+else:
+    # Verificando se a Variável N é Igual a 3
+    if n == 3:
+        # Se a Primeira Condição for Verdadeira Imprimi o Valor na tela
+        print('n é igual a 3')
+    # Se a Primeira Condição for Falsa Executa o Else
+    else:
+        # Imprimindo o Valor na Tela se a Condição for Verdadeira
+        print('n não é igual a 4 nem 3')
+
+
+# Criando Duas Variáveis
+x = 1
+y = 5
+
+# Criando uma Estruta de Condição Utilizando if, else e or
+if (x > 1) or (y % 2 == 0):
+    # Imprimi na Tela se a primeira Condição for Verdadeira
+    print('x é maior que 1 e y é par')
+# Se a Primeira Condição não for Verdadeira Executa o else
+else:
+    # Imprimi na Tela se a Primeira Condição for Falsa
+    print('Uma ou nenhuma das condições foram satisfeitas')
