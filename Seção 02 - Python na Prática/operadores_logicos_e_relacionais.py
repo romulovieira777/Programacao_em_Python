b1dbc4814937acd572fe8774fd4e582e5ff268b6
romulovieira777 <romulo.vieira777@gmail.com>
2020-12-27 12:04:45 -0300

    Python na Prática

---
+++ Seção 02 - Python na Prática/operadores_logicos_e_relacionais.py
@@ -0,0 +1,27 @@
+# Criando Duas Variáveis de Operadores Lógicos
+a = True
+b = False
+
+# Imprimindo as Duas Variáveis na Tela
+print(a, b)
+
+
+# Criando uma Variável
+c = a and b
+
+# Imprimindo na Tela o Resultado das Variáveis
+print("'A' e 'B' são iguais é", c)
+
+
+# Criando uma Variável
+d = a or b
+
+# Imprimindo na Tela o Resultado das Variáveis
+print("'A' ou 'B' é igual a", d)
+
+
+# Criando uma Variável
+e = not b
+
+# Imprimindo na Tela o Resultado das Variáveis
+print('A negacação de B é:', e)
